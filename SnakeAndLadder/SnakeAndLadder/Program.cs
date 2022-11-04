59a2672b7994b9f4d1eafd04dce89fd960c5b587
rajshri-priya <rajshri.priya1708@.gmail.com>
2022-11-04 21:57:01 +0530

    snake and ladder

---
+++ SnakeAndLadder/SnakeAndLadder/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace SnakeAndLadder
+{
+    internal class SnakeAndLadder
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome TO SNAKE AND LADDER Game");
+        }
+
+        public int PlayerPosition = 0;
+    }
+}
